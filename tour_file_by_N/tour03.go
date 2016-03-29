331d99fa0fec6fd7f54d97ea436df9ee193f8f7a
theoneisneo <theoneisneo@gmail.com>
2016-03-29 18:42:16 +0800

    [add] practice 01 ~ 04
    The 1st try of Go lang.

---
+++ tour_file_by_N/tour03.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    fmt.Println("Welcome to tour number 3!")
+
+    fmt.Println("The time is", time.Now())
+}
---
+++ tour_file_by_N/tour04.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("This is a rand number test, I got", rand.Intn(24))
+}
